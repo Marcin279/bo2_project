b63981026ebe07948331db37b5232ba6221f07aa
Marcin279 <marcin.biela12@gmail.com>
2022-01-05 20:08:13 +0100

    replace old pop with new one

--- project/main.py
+++ project/main.py
@@ -78,32 +78,60 @@ def test_tabu_solution():
 
 
 def test_genetic_algo():
-    lista_produktow = np.array([
-        [5.600e-01, 1.335e+03],
-        [2.500e-01, 1.274e+03],
-        [3.800e-01, 6.570e+02],
-        [7.000e-02, 1.258e+03],
-        [1.100e-01, 8.410e+02],
-        [5.500e-01, 1.146e+03],
-        [3.200e-01, 9.850e+02],
-        [2.000e-02, 8.490e+02],
-        [1.400e-01, 1.235e+03],
-        [1.000e-01, 1.343e+03]])
+    # lista_produktow = np.array([
+    #     [5.600e-01, 1.335e+03],
+    #     [2.500e-01, 1.274e+03],
+    #     [3.800e-01, 6.570e+02],
+    #     [7.000e-02, 1.258e+03],
+    #     [1.100e-01, 8.410e+02],
+    #     [5.500e-01, 1.146e+03],
+    #     [3.200e-01, 9.850e+02],
+    #     [2.000e-02, 8.490e+02],
+    #     [1.400e-01, 1.235e+03],
+    #     [1.000e-01, 1.343e+03]])
+    lista_produktow = gen.lista_produktow
 
     terminarz = ds.return_calendar(3, 1, 2022, 15, 1, 2022)
 
     sol = gen.init_population(10, terminarz, lista_produktow)
 
     # for i in range(len(sol)):
-    #   print(sol[i])
+    #   print(sol[i],'\n')
 
     sol_2 = gen.eval_init_population(sol)
 
     # for i in range(len(sol)):
-    #   print(sol_2[i])
+    #   print(sol_2[i],'\n')
 
     rull = gen.rulette(sol_2)
-    gen.print_solution(sol)
+
+    # gen.print_solution(sol)
+    # print('\n')
+    # print(rull[0])
+
+    print('\n')
+    pairs = gen.return_pairs(rull)
+    # for i in range(len(pairs)):
+    #   print(pairs[i],'\n')
+
+    offspring = gen.crossover(pairs)
+    # for i in range(len(offspring)):
+    #   print(offspring[i],'\n')
+
+    # print("lista offspringow po krzyzowaniu", len(offspring))
+    offs_mut = gen.mutation(offspring)
+
+    # gen.print_solution2(offs_mut, title='lista_offspringow po mutacji')
+
+    # for i in range(len(offs_mut)):
+    #     print(gen.check_offspring_singular(offs_mut[i]), '\n')
+
+    offspings_checked = gen.check_offspring(offs_mut)
+    # gen.print_solution2(offspings_checked, title = 'offspings_checked')
+
+    # Nie wiadomo czy dziala rull [[1, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 0]
+    x = gen.replace_old_pop_with_new_one(rull, offspings_checked)
+    print(x)
 
 
 if __name__ == '__main__':
@@ -115,4 +143,4 @@ if __name__ == '__main__':
     # test_check_capacity()
     # test_check_current_solution_in_tabu_list()
     # test_tabu_solution()
-    print(test_genetic_algo())
+    test_genetic_algo()
