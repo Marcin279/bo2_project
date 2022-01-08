977ceda988df32166a6e0c15692f39aa04838c6b
Marcin279 <marcin.biela12@gmail.com>
2022-01-08 11:46:28 +0100

    merge from mbiela branch

---
+++ project/main.py
@@ -0,0 +1,164 @@
+import src.data_structures as ds
+import src.tabu_solution as tsol
+import src.genetic_algo as gen
+
+import numpy as np
+
+
+def test_solution_structures():
+    # OK
+    d = 1
+    prod = np.random.randint(0, 2, 10)
+    bilans = 456.1
+    sol = ds.Solution(d, prod, bilans)
+    print(sol)
+
+
+def test_generate_initial_solution():
+    terminarz = ds.return_calendar(3, 1, 2022, 15, 1, 2022)
+    lista_produktow = ds.generuj_liste_produktow()
+    # print(np.array(lista_produktow))
+    lod1 = tsol.Lodowka(terminarz, lista_produktow)
+
+    lod1.print_solution(lod1.generete_initial_solution())
+
+
+def test_step1():
+    terminarz = ds.return_calendar(3, 1, 2022, 15, 1, 2022)
+    lista_produktow = ds.generuj_liste_produktow()
+    # print(np.array(lista_produktow))
+    lod1 = tsol.Lodowka(terminarz, lista_produktow)
+    sol = lod1.generete_initial_solution()
+    # lod1.print_solution(sol)
+
+    sol = lod1.zwroc_liste_produktow(sol)
+    print('type_sol', type(sol))
+    print('\n\n')
+    sol_step = lod1.step1(sol)
+    print(sol_step)
+    # lod1.print_solution(sol)
+
+
+def test_check_capacity():
+    terminarz = ds.return_calendar(3, 1, 2022, 15, 1, 2022)
+    lista_produktow = ds.generuj_liste_produktow()
+    # print(np.array(lista_produktow))
+    lod1 = tsol.Lodowka(terminarz, lista_produktow)
+    sol = lod1.generete_initial_solution()
+    sol = lod1.zwroc_liste_produktow(sol)
+    sol_step = lod1.step1(sol)
+    ch_cap = lod1.check_capacity(sol_step)
+    print(ch_cap)
+
+
+def test_check_current_solution_in_tabu_list():
+    terminarz1 = ds.return_calendar(3, 1, 2022, 15, 1, 2022)
+    lista_produktow1 = ds.generuj_liste_produktow()
+    lod1 = tsol.Lodowka(terminarz1, lista_produktow1)
+    sol1 = lod1.generete_initial_solution()
+    sol1 = lod1.zwroc_liste_produktow(sol1)
+    sol_step1 = lod1.step1(sol1)
+    sol_step1_1 = lod1.step1(sol_step1)
+    lod1.tabu_list.append(sol_step1)
+    lod1.tabu_list.append(sol_step1_1)
+    print(np.array_equal(sol_step1, sol1))
+    # print(sol1)
+    print(lod1.tabu_list)
+    check = lod1.check_current_sol_in_tabu_list(lod1.tabu_list, sol_step1)
+    print(check)
+
+
+def test_tabu_solution():
+    terminarz = ds.return_calendar(1, 1, 2022, 30, 1, 2022)
+    lista_produktow = ds.generuj_liste_produktow()
+    lod1 = tsol.lodowka(terminarz, lista_produktow)
+    lod1.tabu_solution()
+    lod1.print_solution(lod1.best_solution)
+    print(lod1.zwroc_najlepsze_rozwiazanie(lod1.best_solution))
+
+
+def test_genetic_algo():
+    # lista_produktow = np.array([
+    #     [5.600e-01, 1.335e+03],
+    #     [2.500e-01, 1.274e+03],
+    #     [3.800e-01, 6.570e+02],
+    #     [7.000e-02, 1.258e+03],
+    #     [1.100e-01, 8.410e+02],
+    #     [5.500e-01, 1.146e+03],
+    #     [3.200e-01, 9.850e+02],
+    #     [2.000e-02, 8.490e+02],
+    #     [1.400e-01, 1.235e+03],
+    #     [1.000e-01, 1.343e+03]])
+    lista_produktow = gen.lista_produktow
+
+    terminarz = ds.return_calendar(3, 1, 2022, 15, 1, 2022)
+
+    sol = gen.init_population(10, terminarz, lista_produktow)
+
+    # for i in range(len(sol)):
+    #   print(sol[i],'\n')
+
+    sol_2 = gen.eval_init_population(sol)
+
+    # for i in range(len(sol)):
+    #   print(sol_2[i],'\n')
+
+    rull = gen.rulette(sol_2)
+
+    # gen.print_solution(sol)
+    # print('\n')
+    # print(rull[0])
+
+    print('\n')
+    pairs = gen.return_pairs(rull)
+    # for i in range(len(pairs)):
+    #   print(pairs[i],'\n')
+
+    offspring = gen.crossover(pairs)
+    # for i in range(len(offspring)):
+    #   print(offspring[i],'\n')
+
+    # print("lista offspringow po krzyzowaniu", len(offspring))
+    offs_mut = gen.mutation(offspring)
+
+    # gen.print_solution2(offs_mut, title='lista_offspringow po mutacji')
+
+    # for i in range(len(offs_mut)):
+    #     print(gen.check_offspring_singular(offs_mut[i]), '\n')
+
+    offspings_checked = gen.check_offspring(offs_mut)
+    # gen.print_solution2(offspings_checked, title = 'offspings_checked')
+
+    # Nie wiadomo czy dziala rull [[1, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 0]
+    x = gen.replace_old_pop_with_new_one(gen.pull_parents_form_parents_longer(rull), offspings_checked)
+    print(len(x))
+
+
+
+
+if __name__ == '__main__':
+    # print(ds.return_calendar(3, 1, 2022, 15, 1, 2022))
+    # test_solution_structures()
+
+    # test_generate_initial_solution()
+    # test_step1()
+    # test_check_capacity()
+    # test_check_current_solution_in_tabu_list()
+    # test_tabu_solution()
+    # test_genetic_algo()
+
+    print('dla i = 1000')
+
+    solution = gen.genetic_algo(1000, gen.lista_produktow, ds.return_calendar(3, 1, 2022, 15, 1, 2022), 20, 0.9)
+    print(solution, '\n')
+
+    # print('dla i = 500')
+
+    # solution = gen.genetic_algo(500, gen.lista_produktow, ds.return_calendar(3, 1, 2022, 15, 1, 2022), 20, 0.9)
+    # print(solution)
+
+
+    # lista_produktow = gen.lista_produktow
+    # terminarz = ds.return_calendar(3, 1, 2022, 15, 1, 2022)
+
+    
---
+++ project/src/genetic_algo.py
@@ -0,0 +1,393 @@
+import numpy as np
+import src.data_structures as ds
+# from src.data_structures import
+import copy
+from typing import List, Union, Dict, Tuple, Set, Any
+import random
+from src.tabu_solution import lodowka
+
+lista_produktow = np.array([
+    [5.600e-01, 1.335e+03],
+    [2.500e-01, 1.274e+03],
+    [3.800e-01, 6.570e+02],
+    [7.000e-02, 1.258e+03],
+    [1.100e-01, 8.410e+02],
+    [5.500e-01, 1.146e+03],
+    [3.200e-01, 9.850e+02],
+    [2.000e-02, 8.490e+02],
+    [1.400e-01, 1.235e+03],
+    [1.000e-01, 1.343e+03]])
+
+ograniczenia = ds.Ograniczenia()
+
+
+def print_solution2(data: List[List[int]], title: str = 'XD'):
+    print(title, sep='\n')
+    for i in range(len(data)):
+        tmp = np.array(data[i])
+        print(tmp)
+        print('\n')
+
+
+def print_solution(solution):
+    S = ''
+    for elem in solution:
+        S += f"d: {elem[0]}  lst: {elem[1]}  b: {elem[2]}\n"
+    print(S)
+
+
+# Step 1. Create an initial population of P chromosomes.
+def init_population(n, terminarz, lista_produktow):
+    """
+    Parameters:
+    n - ilość osobników do otrzymania
+    terminarz - lista dni na jakie chcemy zrobić rozpiskę
+    lista_produktow - stała lista produktów z ich wagą i kalorycznością
+    Returns:
+    init_population (list): lista rozwiązań początkowych
+    """
+    init_population = []
+    for i in range(n):
+        lodowka_ = lodowka(terminarz, lista_produktow).initial_solution
+        init_population.append(lodowka_)
+    return init_population
+
+
+# Step 2. Evaluate the fitness of each chromosome.
+def evaluate_chromosome(solution):
+    min = 0
+    for i in range(len(solution)):
+        min += solution[i][0]
+    return min
+
+
+def eval_init_population(init_population):  # wyliczenie wartosci funkcji celu dla osobnika
+    evaluated_init_population = []
+    # lista_pomocnicza = []
+    for i in range(len(init_population)):
+        evaluated_init_population.append((init_population[i], evaluate_chromosome(init_population[i])))
+        # lista_pomocnicza.append(evaluate_chromosome(init_population[i]))
+    # print(lista_pomocnicza)
+
+    return evaluated_init_population
+
+
+# Step 3. Choose P/2 parents from the current population via proportional selection.
+def rulette(evaluated_init_population, len_init_population):
+    """
+    zwraca lista[decyzja, lista_prod, bilnas]
+    :param evaluated_init_population:
+    :return: lista[decyzja, lista_prod, bilnas]
+    """
+
+    parents_list = []
+    ite = 0
+    list_eval = copy.deepcopy(evaluated_init_population)
+
+    # print(evaluated_init_population)
+    # lista_pomocnicza = []
+    # for i in range(len(evaluated_init_population)):
+    #     lista_pomocnicza.append((evaluated_init_population[i][1]))
+    # print(lista_pomocnicza)
+    # print(evaluated_init_population[0])
+
+    how_many = len_init_population // 2
+    if how_many % 2 != 0:
+        how_many += 1
+
+    lista_pomocnicza2 = []
+    while ite < how_many:
+        ite += 1
+        elem = list_eval.pop(0)
+        parents_list.append(elem[0])
+        lista_pomocnicza2.append(elem[1])
+
+    #     suma_po_wyjsciach = 0
+    #     for i in range(len(list_eval)):
+    #         suma_po_wyjsciach += list_eval[i][1]
+
+    #     stop_points = [0]
+    #     for j in range(len(list_eval)):
+    #         stop_points.append(1 / list_eval[j][1])
+    #     sum_stop_points = sum(stop_points)
+    #     normalized_stop_points = [point / sum_stop_points for point in stop_points]
+    #     normalized_stop_points_summed = [normalized_stop_points[0]]
+    #     for i in range(1, len(normalized_stop_points)):
+    #         normalized_stop_points_summed.append(normalized_stop_points_summed[i - 1] + normalized_stop_points[i])
+
+    #     rand = random.random()
+    #     chosen_point = 0  # indeks rodzica
+    #     for i in range(len(normalized_stop_points_summed) - 1):
+    #         if normalized_stop_points_summed[i] <= rand < normalized_stop_points_summed[i + 1]:
+    #             chosen_point = i
+    #             break
+    #         else:
+    #             continue
+    #     parents_list.append(list_eval.pop(chosen_point)[0])
+    # print(parents_list[0])
+    # print(lista_pomocnicza2)
+    return parents_list
+
+
+# Step 4. Randomly select two parents to create offspring using crossover operator.
+def return_pairs(parents_list):
+    pairs_list = []
+    for i in range(0, len(parents_list) - 1, 2):
+        pairs_list.append((parents_list[i], parents_list[i + 1]))
+
+    # print('len(pairs_list)',len(pairs_list))
+    return pairs_list
+
+
+def crossover(pairs_list):
+    offsprings_list = []
+    for i in range(len(pairs_list)):
+        # Step 6. Repeat Steps  4 and 5 until all parents are selected and mated.
+        cutpoint = random.randint(1, len(pairs_list[i][0]) - 1)
+        offspring1 = []
+        offspring2 = []
+        for j in range(len(pairs_list[i][0])):
+            if j < cutpoint:
+                offspring1.append(pairs_list[i][0][j][1])
+                offspring2.append(pairs_list[i][1][j][1])
+            else:
+                offspring1.append(pairs_list[i][1][j][1])
+                offspring2.append(pairs_list[i][0][j][1])
+        offsprings_list.append(offspring1)
+        offsprings_list.append(offspring2)
+    return offsprings_list
+
+
+# Step 5. Apply mutation operators for minor changes in the results.
+def mutation_singular(macierz_pom_produktow, ile_zamian=None):
+    macierz_pom_produktow2 = np.array(macierz_pom_produktow)
+
+    if ile_zamian == None:
+        ile_zamian = int(macierz_pom_produktow2.shape[0] / 7 * 30)
+
+    for i in range(ile_zamian):
+        kierunek_przesuniecia = [1, 2, 3, 4]  # 1 - gora, 2 - dol, 3-lewo, 4-
+        x_idx = np.random.randint(0, macierz_pom_produktow2.shape[1])
+        y_idx = np.random.randint(0, macierz_pom_produktow2.shape[0])
+        if x_idx == 0:
+            kierunek_przesuniecia.remove(4)
+        elif x_idx == macierz_pom_produktow2.shape[1] - 1:
+            kierunek_przesuniecia.remove(2)
+
+        if y_idx == 0:
+            kierunek_przesuniecia.remove(1)
+
+        elif y_idx == macierz_pom_produktow2.shape[0] - 1:
+            kierunek_przesuniecia.remove(3)
+
+        kierunek_przesuniecia_wybor = random.choice(kierunek_przesuniecia)
+        if kierunek_przesuniecia_wybor == 1:  # gora
+            macierz_pom_produktow[y_idx][x_idx], macierz_pom_produktow[y_idx - 1][x_idx] = macierz_pom_produktow[
+                                                                                               y_idx - 1][x_idx], \
+                                                                                           macierz_pom_produktow[
+                                                                                               y_idx][x_idx]
+        elif kierunek_przesuniecia_wybor == 3:  # dol
+            macierz_pom_produktow[y_idx][x_idx], macierz_pom_produktow[y_idx + 1][x_idx] = macierz_pom_produktow[
+                                                                                               y_idx + 1][x_idx], \
+                                                                                           macierz_pom_produktow[
+                                                                                               y_idx][x_idx]
+
+        elif kierunek_przesuniecia_wybor == 2:  # prawo
+            macierz_pom_produktow[y_idx][x_idx], macierz_pom_produktow[y_idx][x_idx + 1] = macierz_pom_produktow[
+                                                                                               y_idx][x_idx + 1], \
+                                                                                           macierz_pom_produktow[
+                                                                                               y_idx][x_idx]
+        else:  # lewo
+            macierz_pom_produktow[y_idx][x_idx], macierz_pom_produktow[y_idx][x_idx - 1] = macierz_pom_produktow[
+                                                                                               y_idx][x_idx - 1], \
+                                                                                           macierz_pom_produktow[
+                                                                                               y_idx][x_idx]
+
+    return macierz_pom_produktow
+
+
+def mutation(offsprings_list, prob_od_mut=0.1, changes_no=None):
+    offsprings_list_mutated = []
+    for i in range(len(offsprings_list)):
+        mutation = random.random()
+        if mutation <= prob_od_mut:
+            mutated = mutation_singular((offsprings_list[i]), changes_no)
+            offsprings_list_mutated.append(mutated)
+        else:
+            offsprings_list_mutated.append((offsprings_list[i]))
+    return offsprings_list_mutated
+
+
+# !! napisac w dokumentacji o dostosowaniu plecaka do wag
+
+# po mutacji mozemy  sprawdzac poprawnosc otrzymanego osobnika (czy spelnia zalozenia)
+# jezeli nie to mozemy
+# a) odrzucic go i do wynikowej listy osobnikow (nowej populacji) go nie wpisywac
+#   -> zeby populacja sie nie zmniejszala mozemy ze starej populacji podmmieniac te osobniki o najgorszej wartosci f celu
+# b) mozemy go probowac poprawiac (jezeli brakuje kalorii do dodawac produkty) -> zeby nie bylo problemu to mozemy zwiekszyc ktores z ograniczen
+# z kotrego bysmy nie skorzystali rozwiazania trzeba bedzie dokladnie opisac w dokumenacji, bo to nie jest zbyt "standardowe" rozwiazanie
+
+def check_offspring_singular(offspring):
+    """
+    jako parametr przyjmuje macierz w której wiersze reprezentują kolejne dni, natomiast
+    kolumny listę produktów
+    Zwraca bool - czy osobnik spelnia zalozenia, czy nie
+    """
+
+    ponad_stan_lst_lodowka = []
+    bilans_kalorie = []
+    aktualny_stan_lodowki = ograniczenia.poczatkowy_stan_lodowki
+    zawartosc_lodowki = []
+    for row in range(len(offspring)):
+
+        if not all([v == 0 for v in offspring[row]]):
+            zawartosc_lodowki.append(offspring[row])
+            aktualny_stan_lodowki += sum(offspring[row])
+            ponad_stan_lst_lodowka.append(aktualny_stan_lodowki)
+
+        aktualne_zuzycie = 0
+        bilans_kalorie.append(0)
+        while aktualne_zuzycie < ograniczenia.zapotrz_kal:
+            if (len(np.nonzero(zawartosc_lodowki)[0])) > 0:
+                id_row = np.nonzero(zawartosc_lodowki)[0][0]
+                id_col = np.nonzero(zawartosc_lodowki)[1][0]
+                jedzony_produkt = lista_produktow[id_col]
+
+                zawartosc_lodowki[id_row][id_col] = 0
+                kalorycznosc = jedzony_produkt[1]
+                aktualne_zuzycie += kalorycznosc
+                aktualny_stan_lodowki -= 1
+
+            else:
+                bilans_kalorie.append(aktualne_zuzycie - ograniczenia.zapotrz_kal)
+                break
+    if all([elem <= ograniczenia.maksymalna_poj_lodowki for elem in
+            ponad_stan_lst_lodowka]) and all([elem == 0 for elem in bilans_kalorie]):
+        return 1
+    return 0
+
+
+def check_offspring(offsprings_list_mutated):
+    offsprings_checked = []
+    for i in range(len(offsprings_list_mutated)):
+        offs_copy = copy.deepcopy(offsprings_list_mutated[i])
+        offspring_ok = check_offspring_singular(offs_copy)
+
+        if offspring_ok:
+            offsprings_checked.append(offsprings_list_mutated[i])
+        else:
+            continue
+    return offsprings_checked
+
+
+# Step 7. Replace old population of chromosomes with new one.
+
+def if_row_has_zero(row: List[int]) -> int:
+    if any(row):
+        return 1
+    else:
+        return 0
+
+
+def evaluate_1(individual: List[List[int]]):
+    sum = 0
+    for elem in individual:
+        sum += if_row_has_zero(elem)
+    return sum
+
+
+# Step 8. Evaluate the fitness of each chromosome in the new population.
+def replace_old_pop_with_new_one(old_population: List[List[List[int]]], new_population: List[List[List[int]]]):
+    result_for_old = []
+    for elem in old_population:
+        pair = elem, evaluate_1(elem)
+        result_for_old.append(pair)
+
+    result_for_new = []
+    for elem in new_population:
+        pair = elem, evaluate_1(elem)
+        result_for_new.append(pair)
+
+    nowa_lista = []
+    for i in range(0, len(result_for_new)):
+        nowa_lista.append(result_for_new[i])
+    for i in range(0, len(result_for_old)):
+        nowa_lista.append(result_for_old[i])
+
+    nowa_lista = sorted(nowa_lista, key=lambda t: t[1], reverse=False)
+
+    return nowa_lista
+
+
+def pull_parents_form_parents_longer(old_population):
+    parents = []
+    for i in range(len(old_population)):
+        parents.append([])
+        for j in range(len(old_population[i])):
+            parents[i].append(old_population[i][j][1])
+    return parents
+
+
+# Step 9. Terminate if the number of generations meets some upper bound; otherwise go to Step3.
+
+def change_new_popul_to_other_format(new_population):
+    formatted = []
+
+    for elem in (new_population):
+        list = []
+        for j in range(len(elem[0])):
+            decision = if_row_has_zero(elem[0][j])
+            list.append([decision, elem[0][j], 0])
+        formatted.append(list)
+
+    return formatted
+
+
+def genetic_algo(upper_bound, lista_produktow, terminarz, len_init_population, probability=0.1, liczba_zamian=None):
+    # osobniki poczatkowe
+    init_specimen = init_population(len_init_population, terminarz, lista_produktow)
+
+    i = 0
+    while i < upper_bound:
+        # osobniki poczatkowe plus wartosc f celu
+        init_specimen_f_celu = eval_init_population(init_specimen)
+
+        # zwraca połowę poczatkowych osobnikow przez losowanie
+        # prawdopodobienstwo wybrania osobnika do krzyzowania jest odwrotnie proporcjonalne do wartosci funkcji celu
+        # (minimalizujemy jej wartosc)
+        rull = rulette(init_specimen_f_celu, len_init_population)
+        copy_rull = copy.deepcopy(rull)
+        # print('rull')
+
+        # print(rull)
+
+        # łaczenie wybranych w losowaniu osobników w pary
+        pairs = return_pairs(rull)
+
+        # krzyżowanie rodziców - nowa populacja
+        offspring = crossover(pairs)
+
+        # mutowanie osobników z prawdopodobienstwem wystapienia mutacji = probability (wartosc domyslna  = 0.1)
+        # oraz liczba zamian przy niej wykonywanych = liczba_zamian (wartosc domysna ~= 30 zamian na tydzień)
+        offs_mut = mutation(offspring, prob_od_mut=probability, changes_no=liczba_zamian)
+
+        # sprawdzenie poprawności otrzymanych po krzyzowaniu i mutacji osobników (bilans kaloryczny oraz upper bound lodowki)
+        offspings_checked = check_offspring(offs_mut)
+
+        # dodanie do starych osobnikow, nowo powstalych
+        x = replace_old_pop_with_new_one(pull_parents_form_parents_longer(copy_rull), offspings_checked)
+
+        # print('x')
+        # print(x)
+
+        print('iteracja = ', i, 'f.celu = ', x[0][1])
+
+        # len(x) jest miedzy [len(rodzice), 2*len(rodzice)]
+        init_specimen = change_new_popul_to_other_format(x)
+
+        # print('init_specimen')
+        # print(init_specimen)
+
+        i += 1
+
+    return x
